313db5c63a556d7baf99b72ea6c1041a551ea5f9
DesarrolloProsis <38117134+DesarrolloProsis@users.noreply.github.com>
2018-04-11 17:03:35 -0500

    Prueba

--- WebAPITutorial/App_Start/WebApiConfig.cs
+++ WebAPITutorial/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace WebAPITutorial
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "services/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ WebAPITutorial/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebAPITutorial.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int UnidadesEnAlmacen { get; set; }
+    }
+
+    public class ProductoDbContext : DbContext
+    {
+        public DbSet<Producto> Producto { get; set; }        
+    }
+}
